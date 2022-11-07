d83773adeafe31f94c307dc94e6a9ba50d2b7be1
kbugayenko <k.bugayenko@gmail.com>
2022-11-07 19:36:02 +0200

    udpate 1+2 html

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.addEventListener("click", onClickGallery);
+
+const markup = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+        </a>
+    </div>
+    `
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+function onClickGallery(evt) {
+  evt.preventDefault();
+
+  if (evt.target.className == "gallery__image") {
+    basicLightbox
+      .create(
+        `
+        <img src="${evt.target.dataset.source}" width="800" height="600">
+        `
+      )
+      .show();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.addEventListener("click", onClickGallery);
+
+const markup = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `
+    <a class="gallery__item" style="display: block" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
+
+function onClickGallery(evt) {
+  evt.preventDefault();
+  gallery.on("show.simplelightbox", function () {});
+}
